a070e3f058426f666eeb3d111f6d6dd5c68594ed
kondakings <kondareddyavuduri@gmail.com>
2024-03-05 18:54:39 +0000

    add this

--- Gmail/tests/gmailtest.js
+++ Gmail/tests/gmailtest.js
@@ -11,7 +11,7 @@ test ('whill', async t => {
     await t.click(gmailpage.login);
     await t.typeText(gmailpage.uname,"Reddytest");
     await t.typeText(gmailpage.pass,"Reddy@1996");
-   await t.click(gmailpage.check);
+    await t.click(gmailpage.check);
     await t.click(gmailpage.signin);
     await t.wait(10000);
 
